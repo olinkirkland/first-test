05c128285693f5ea55d0eefd268717da3401d070
Olin Kirkland <olin.kirkland@beg.de>
2024-04-25 12:46:43 +0200

    Initi commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>
+    {{ time }}
+  </h1>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+// Javascript here
+
+const time = ref(new Date().toLocaleTimeString());
+setInterval(() => {
+  time.value = new Date().toLocaleTimeString();
+}, 1000);
+
+</script>
